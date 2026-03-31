7e3ba3f7c379a3c518d8fc9d225c99fc65d50a50
Shrinidhi Holla <hollashrinidhi99@gmail.com>
2026-04-01 00:58:56 +0530

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,7 @@
    ══════════════════════════════════════════════════════════════════════ */
 
 // ── CONFIG ────────────────────────────────────────────────────────────────
-const WS_URL   = 'business-game-backend-production.up.railway.app/ws';  // change to your server URL
+const WS_URL   = 'http://business-game-backend-production.up.railway.app/ws';  // change to your server URL
 const APP_PRE  = '/app';
 const TOPIC    = '/topic';
 
